b4cb1ac6edcf338841d2836bc4f9cb873af9bae5
DOGAN <88912764+doganemrex@users.noreply.github.com>
2021-12-06 17:13:08 +0300

    Add files via upload

---
+++ photo.php
@@ -0,0 +1,9 @@
+<?php
+require 'class.php';
+$username = "doganxreal";
+$url2="https://smihub.com/search?query=$username";
+$ip=str_get_html(file_get_contents($url2));
+$pp=$ip->find("img[class='img-fluid w-100']",0)->src;
+
+
+echo $pp;
